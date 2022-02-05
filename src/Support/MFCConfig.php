4c2a65b68e970dd2ac22d7014f93230f43fc8bb7
Klym <klimssalex@gmail.com>
2022-02-05 16:57:34 +0200

    new (MFCConfig): added get path methods

--- src/Support/MFCConfig.php
+++ src/Support/MFCConfig.php
@@ -4,5 +4,18 @@ namespace EvolutionCMS\MFConfigure\Support;
 
 class MFCConfig
 {
+    public static function getMFTVsPath(): string
+    {
+        return MODX_BASE_PATH.'/assets/plugins/multifields/config/';
+    }
 
+    public static function getFilesPath():string
+    {
+        return __DIR__.'/files/';
+    }
+
+    public static function getTVPlaceholder(): string
+    {
+        return '[+TV_NAME+]';
+    }
 }
