20040c5f08092b49baf42e6b8d74bb2747c67791
Klym <klimssalex@gmail.com>
2022-02-05 17:17:56 +0200

    fix (configExample): dynamically tvName

--- src/Support/files/configExample.php
+++ src/Support/files/configExample.php
@@ -1,3 +1,3 @@
 <?php
 
-return (new \EvolutionCMS\MFConfigure\Managers\MFConfigureManager())->getConfig(basename(__FILE__));
+return (new \EvolutionCMS\MFConfigure\Managers\MFConfigureManager())->getConfig(basename(__FILE__,'.php'));
